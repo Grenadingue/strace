f0c9966235b42223cbb9b3ddf106f93cf3b95ea7
chauvi_n <chauvi_n@epitech.eu>
2015-04-08 23:14:58 +0200

    fix a mistake for 'long int' VS 'long long int' for compilation on debian VS archlinux

--- source/print_spe.c
+++ source/print_spe.c
@@ -5,7 +5,7 @@
 ** Email <chauvin.nico@gmail.com>
 **
 ** Started on  Sun May 18 19:23:06 2014 Nicolas Chauvin
-** Last update Wed Apr  8 23:02:15 2015 Nicolas Chauvin
+** Last update Wed Apr  8 23:13:33 2015 Nicolas Chauvin
 */
 
 #include <stdbool.h>
@@ -28,7 +28,7 @@ void		print_va_arg(bool comma_flag, t_reg_value value)
 void		print_hexa(bool comma_flag, t_reg_value value)
 {
   if (sizeof(t_reg_value) == sizeof(unsigned long long int))
-    printf("%s0x%016llX", comma_flag ? ", " : "", value);
+    printf("%s0x%016llX", comma_flag ? ", " : "", (unsigned long long int)value);
   else
     printf("%s0x%016lX", comma_flag ? ", " : "", (unsigned long int)value);
 }
